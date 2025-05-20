dad6f85359ef9b7a4571abaaf06626a2199467ec
Uday Saini <uday10saini@gmail.com>
2025-05-20 01:02:39 -0400

    scaffolding for router, store, composables, views, components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// Install Pinia and Router and then mount the app
+app.use(createPinia())
+app.use(router)
+app.mount('#app')

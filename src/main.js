45b561217c88158b2c26b9e3ebc4428a8e2c58e3
Uday Saini <uday10saini@gmail.com>
2025-05-22 00:25:30 -0400

    removed primevue dead code.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
 
 import './style.css'
 import App from './App.vue'
-
-// Import PrimeVue components
-import Button from "primevue/button"
-import SelectButton from 'primevue/selectbutton'
-import Listbox from 'primevue/listbox';
-import InputGroup from 'primevue/inputgroup';
-import InputGroupAddon from 'primevue/inputgroupaddon';
-import InputText from 'primevue/inputtext';
-import InputNumber from 'primevue/inputnumber';
 
 const app = createApp(App)
 
@@ -22,29 +11,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-// Plug in PrimeVue and set the theme
-app.use(PrimeVue, {
-    ripple: true,
-    // inputVariant: 'filled',
-    theme: {
-        preset: Aura,
-        options: {
-            // Add any theme options here
-            cssLayer: {
-                name: 'primevue',
-                order: 'theme, base, primevue'
-            }
-        }
-    }
-})
-
-// Register PrimeVue components globally
-app.component("Button", Button)
-app.component("SelectButton", SelectButton)
-app.component("Listbox", Listbox)
-app.component("InputGroup", InputGroup)
-app.component("InputGroupAddon", InputGroupAddon)
-app.component("InputText", InputText)
-app.component("InputNumber", InputNumber)
 // Mount the app
 app.mount('#app')
